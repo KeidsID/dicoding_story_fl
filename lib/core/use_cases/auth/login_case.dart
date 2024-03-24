131a8260b5a229c53282d0047f8020b48fd87c12
KeidsID <keidscode@gmail.com>
2024-03-24 16:49:47 +0800

    refactor: rename "domain" into "core".

--- lib/core/use_cases/auth/login_case.dart
+++ lib/core/use_cases/auth/login_case.dart
@@ -1,5 +1,5 @@
-import 'package:dicoding_story_fl/domain/entities.dart';
-import 'package:dicoding_story_fl/domain/repos.dart';
+import 'package:dicoding_story_fl/core/entities.dart';
+import 'package:dicoding_story_fl/core/repos.dart';
 
 /// {@template dicoding_story_fl.domain.use_cases.auth.LoginCase}
 /// Use case for logging in.
