de7ee162be2507c83627aa4bca20b9944fc04ea0
KeidsID <keidscode@gmail.com>
2024-03-24 16:50:28 +0800

    feat(infrastructures): add dependencies container.

---
+++ lib/infrastructures/container.dart
@@ -0,0 +1,18 @@
+/// Container for locating a dependencies.
+///
+/// Call [init] before using [get].
+///
+/// ```dart
+/// import 'package:dicoding_story_fl/infrastructures/container.dart' as container;
+///
+/// void main() async {
+///   await container.init();
+///
+///   container.get<DependencyType>();
+/// }
+/// ```
+library container;
+
+import 'container/container.dart' show init, get;
+
+export 'container/container.dart';
