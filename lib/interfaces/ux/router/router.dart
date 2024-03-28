88d8ee6bbe33dabf6b98d525f1ed5072f8e8877c
KeidsID <keidscode@gmail.com>
2024-03-28 14:04:18 +0800

    fix(interfaces): fix invalid stories pagination.

--- lib/interfaces/ux/router/router.dart
+++ lib/interfaces/ux/router/router.dart
@@ -1,13 +1,33 @@
+import 'package:dicoding_story_fl/core/entities.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 import 'package:dicoding_story_fl/container.dart' as container;
+import 'package:dicoding_story_fl/interfaces/ui.dart';
 import 'package:dicoding_story_fl/interfaces/ux.dart';
-import 'package:provider/provider.dart';
 
 final router = GoRouter(
   routes: $appRoutes,
   initialLocation: const LoginRoute().location,
   debugLogDiagnostics: true,
+  errorBuilder: (context, state) {
+    return Scaffold(
+      body: SizedErrorWidget.expand(
+        error: SimpleHttpException(
+          statusCode: 404,
+          message: 'No route found for "${state.uri.path}"',
+        ),
+        action: Builder(builder: (context) {
+          return ElevatedButton.icon(
+            onPressed: () => const StoriesRoute().go(context),
+            icon: const Icon(Icons.home_outlined),
+            label: const Text('Back to Home'),
+          );
+        }),
+      ),
+    );
+  },
   refreshListenable: container.get<AuthProvider>(),
   redirect: (context, state) {
     final currentRoute = state.uri.path;
