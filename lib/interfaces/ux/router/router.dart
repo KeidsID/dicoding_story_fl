f39e6974198706abeaff9656cfad5e4e9e806391
KeidsID <keidscode@gmail.com>
2024-03-28 17:47:39 +0800

    refacctor: dart format lib

--- lib/interfaces/ux/router/router.dart
+++ lib/interfaces/ux/router/router.dart
@@ -9,46 +9,47 @@ import 'package:dicoding_story_fl/interfaces/ux.dart';
 /// Key that store the [router] state.
 final routerKey = GlobalKey<NavigatorState>();
 
-GoRouter router (BuildContext context) => GoRouter(
-  navigatorKey: routerKey,
-  routes: $appRoutes,
-  initialLocation: const LoginRoute().location,
-  debugLogDiagnostics: true,
-  errorBuilder: (context, state) {
-    return Scaffold(
-      body: SizedErrorWidget.expand(
-        error: SimpleHttpException(
-          statusCode: 404,
-          message: 'No route found for "${state.uri.path}"',
-        ),
-        action: Builder(builder: (context) {
-          return ElevatedButton.icon(
-            onPressed: () => const StoriesRoute().go(context),
-            icon: const Icon(Icons.home_outlined),
-            label: const Text('Back to Home'),
-          );
-        }),
-      ),
+GoRouter router(BuildContext context) => GoRouter(
+      navigatorKey: routerKey,
+      routes: $appRoutes,
+      initialLocation: const LoginRoute().location,
+      debugLogDiagnostics: true,
+      errorBuilder: (context, state) {
+        return Scaffold(
+          body: SizedErrorWidget.expand(
+            error: SimpleHttpException(
+              statusCode: 404,
+              message: 'No route found for "${state.uri.path}"',
+            ),
+            action: Builder(builder: (context) {
+              return ElevatedButton.icon(
+                onPressed: () => const StoriesRoute().go(context),
+                icon: const Icon(Icons.home_outlined),
+                label: const Text('Back to Home'),
+              );
+            }),
+          ),
+        );
+      },
+      refreshListenable: context.read<AuthProvider>(),
+      redirect: (context, state) {
+        final currentRoute = state.uri.path;
+        final authProv = context.read<AuthProvider>();
+
+        if (authProv.isLoading) return null;
+
+        final isLoggedIn = authProv.value != null;
+        final isAuthRoute =
+            currentRoute.startsWith(const LoginRoute().location);
+
+        if (isAuthRoute) {
+          if (isLoggedIn) return const StoriesRoute().location;
+
+          return null;
+        }
+
+        if (!isLoggedIn) return const LoginRoute().location;
+
+        return null;
+      },
     );
-  },
-  refreshListenable: context.read<AuthProvider>(),
-  redirect: (context, state) {
-    final currentRoute = state.uri.path;
-    final authProv = context.read<AuthProvider>();
-
-    if (authProv.isLoading) return null;
-
-    final isLoggedIn = authProv.value != null;
-    final isAuthRoute = currentRoute.startsWith(const LoginRoute().location);
-
-    if (isAuthRoute) {
-      if (isLoggedIn) return const StoriesRoute().location;
-
-      return null;
-    }
-
-    if (!isLoggedIn) return const LoginRoute().location;
-
-    return null;
-  },
-);
