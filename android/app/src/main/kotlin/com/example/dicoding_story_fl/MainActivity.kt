95e13360a1d9058b12d77c344bd34b913d8939c6
KeidsID <keidscode@gmail.com>
2024-04-26 15:29:26 +0800

    build: rename app name and bundle id

--- android/app/src/main/kotlin/com/example/dicoding_story_fl/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dicoding_story_fl
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/tech/keids/dicoding_story_fl/MainActivity.kt
@@ -0,0 +1,5 @@
+package tech.keids.dicoding_story_fl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {}
