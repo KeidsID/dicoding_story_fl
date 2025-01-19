b3ce850d50600802f2a7f2cc648f1723910c7adb
KeidsID <keidscode@gmail.com>
2025-01-20 01:53:19 +0800

    docs: update docs ds-11

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     // Restricted Google Maps API key
-    // Please notice if this key still can be used on other apps
+    // Please report if this key still can be used on other apps
     GMSServices.provideAPIKey("AIzaSyDVlWW9LW3Sz7_4tMQDb8jR1EBoQLH0zeA")
 
     GeneratedPluginRegistrant.register(with: self)
